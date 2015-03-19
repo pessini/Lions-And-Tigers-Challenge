52be3ccf0a3bcf6c59dcda6de86eea8ad2befcef
Leandro Pessini <pessini@Leandros-MacBook-Air.local>
2015-03-18 22:54:30 -0300

    Add 3 photos of lions and 3 of tigers

---
+++ LionsAndTigers/CustomCollectionViewCell.h
@@ -0,0 +1,14 @@
+//
+//  CustomCollectionViewCell.h
+//  LionsAndTigers
+//
+//  Created by Leandro Pessini on 3/18/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomCollectionViewCell : UICollectionViewCell
+
+@property (weak, nonatomic) IBOutlet UIImageView *gridImageView;
+@end
---
+++ LionsAndTigers/CustomCollectionViewCell.m
@@ -0,0 +1,13 @@
+//
+//  CustomCollectionViewCell.m
+//  LionsAndTigers
+//
+//  Created by Leandro Pessini on 3/18/15.
+//  Copyright (c) 2015 Brazuca Labs. All rights reserved.
+//
+
+#import "CustomCollectionViewCell.h"
+
+@implementation CustomCollectionViewCell
+
+@end
